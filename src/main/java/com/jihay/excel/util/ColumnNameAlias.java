18e48b3c0fdc82a5f3456ed05e09fe9c7aa1c47d
allen <710240507@qq.com>
2018-10-17 13:05:06 +0800

    init

--- src/main/java/com/jihay/excel/util/ColumnNameAlias.java
+++ src/main/java/com/jihay/excel/util/ColumnNameAlias.java
@@ -7,17 +7,17 @@ public class ColumnNameAlias {
     public static Map<String, List<String>> columnNameAlias = new HashMap<String, List<String>>();
 
     static {
-        columnNameAlias.put("品名", Arrays.asList(new String[]{"品名", "名称", "描述", "品种", "产地名称", "品种板/卷"}));
+        columnNameAlias.put("品名", Arrays.asList("品名", "名称", "描述", "品种", "产地名称", "品种板/卷"));
 
-        columnNameAlias.put("规格", Arrays.asList(new String[]{"规格", "详细说明", "厚度"}));
+        columnNameAlias.put("规格", Arrays.asList("规格", "详细说明", "厚度"));
 
-        columnNameAlias.put("材质", Arrays.asList(new String[]{"材质", "牌号"}));
+        columnNameAlias.put("材质", Arrays.asList("材质", "牌号"));
 
-        columnNameAlias.put("产地", Arrays.asList(new String[]{"产地", "钢厂"}));
+        columnNameAlias.put("产地", Arrays.asList("产地", "钢厂"));
 
-        columnNameAlias.put("价格", Arrays.asList(new String[]{"价格", "单价", "挂牌价"}));
+        columnNameAlias.put("价格", Arrays.asList("价格", "单价", "挂牌价", "销售单价", "理算价", "过磅价格"));
 
-        columnNameAlias.put("仓库", Arrays.asList(new String[]{ "仓库*", "存放地*", "所在地*", "存放仓库"}));
+        columnNameAlias.put("仓库", Arrays.asList("仓库*", "存放地*", "所在地*", "存放仓库"));
     }
 
 }
